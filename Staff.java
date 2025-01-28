51351317d166cdcbb32b514d63e8cd917e4f8943
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2025-01-28 12:47:17 +0300

    Create Staff.java

---
+++ Staff.java
@@ -0,0 +1,18 @@
+Package com; 
+ 
+public class Staff extends Person { 
+    private String staffId; 
+ 
+    public Staff(String id, String name, String staffId) { 
+        super(id, name); 
+        this.staffId = staffId; 
+    } 
+ 
+    public String getStaffId() { 
+        return staffId; 
+    } 
+ 
+    public void setStaffId(String staffId) { 
+        this.staffId = staffId; 
+    } 
+}
